1c6dad185ad3d8f1bc23cfa0b3e57c9463d3d121
momentsnotice <momentsnoticecontact@gmail.com>
2020-02-05 14:51:48 +0900

    2 posts

--- src/components/posts/0.js
+++ src/components/posts/0.js
@@ -6,10 +6,10 @@ class App extends React.Component {
     return (
       <Post>
         <EntryTitle>
-          How to make the Seoul Metropolitan Subway (or any subway at that) better (Test Excerpt)
+          Making the Seoul Metro (or any metro) Better || Part 1
         </EntryTitle>
         <Date>
-          Published January 14, 2020
+          Written by Sean Rhee || January 14, 2020
         </Date>
         <Image src={require('./subway_map.jpeg')}/>
         <Caption>
@@ -40,7 +40,7 @@ class App extends React.Component {
           Service potential is, in essence, the "importance" of a station. Although this can be determined many ways, the way that we'll be determining this is by creating a list of the average traffic values of every station and normalizing them across some range. The higher the service potential, the more time-optimized that station will be. The lower the service potential, the more distance-optimized that station will be.
         </MainText>
         <MainText>
-          Assigning a metro system an efficiency ultimately comes down to relating these three values of distance, commuter time, and service potential in a reasonable, quantifiable way. Using this newly derived efficiency value, we can then train a machine learning algorithm to output metro layouts and grade their fitness. Ultimately, this should result in us receiving a theoretically better map for the Seoul metro.
+          Assigning a metro system an efficiency ultimately comes down to relating these three values of distance, commuter time, and service potential in a reasonable, quantifiable way. Using this newly derived efficiency value, we can then train a machine learning algorithm to output metro layouts and grade their fitness. Ultimately, this should result in us receiving a theoretically better map for the Seoul metro. I say theoretically because while improving Seoul's subway is a long-term goal, for the purposes of this post, I'll be focusing more on the methodology and verification of its validity.
         </MainText>
         <Subtitle>
           Method and Reasoning
@@ -56,23 +56,29 @@ class App extends React.Component {
           A Basic Visualization of How this Graphical Representation Works
         </Caption>
         <MainText>
-          Using this graphical representation of a subway system, we can finally obtain our inefficiency metric by summing the minimum distances from each node to every other node using an SPF algorithm (such as Dijkstra's algorithm or the Floyd Warshall algorithm), weighting each sum based on the start node's service potential, and averaging these weighted sums.
+          Using this graphical representation of a subway system, we can finally obtain our inefficiency metric by iterating through each vertex, weighting the INP and NTP by our vertex's service potential, summing the minimum distances from each node to every other node using an SPF algorithm (such as Dijkstra's algorithm or the Floyd Warshall algorithm), re-weighting each sum based on the start node's service potential, and averaging these weighted sums.
         </MainText>
         <MainText>
-          Initially, this entire methodology may seem somewhat contrived, but with some thought, this metric should actually make a lot of intuitive sense. First of all, in the example provided above, it's easy to see how if all the vertices labeled "2" (the only transfer station depicted) were collapsed and combined our chain model would resemble its real-life counterpart. If the NTPs and INPs had weights of 0, our new chain model would result in a distance-optimized graph (and we'd have a minimum spanning tree). Likewise, if you imagine that the INPs and NTPs were far larger, it would make the extra edge weight drawn from real-life distance trivial thereby making it more important to avoid any stops/transfers than anything else leading to a commuter time–optimized graph (and we'd have a complete graph). Finally, thinking back to earlier, "the higher the service potential, the more time-optimized that station will be. The lower the service potential, the more distance-optimized that station will be." By weighting our sums based on service potential, we fulfill this goal. Imagine if station service potentials were
+          Initially, this entire methodology may seem somewhat contrived, but with some thought, it should actually make a lot of intuitive sense. First, by using a station's traffic as its service potential, we can ensure that "more important" stations are more commuter time–optimized and are more prioritized while the "less important" stations are more distance-optimized and are less prioritized. This is because by weighting INPs and NTPs by service potential, a high service potential would result in the incentivization of visiting fewer stops and making fewer transfers. Additionally, by weighting our sum by service potential, minimizing our weighted sum for "more important" stations will matter more (which makes sense). The opposite applies for stations with low service potential. Also, the reason our abstracted chain representation even works to begin with is because a) if you imagine that our INPs and NTPs in the figure above were of length 0, our chain model would resemble its real-life counterpart and b) if you think about it, the reason it's annoying to have lots of stops or transfers is because in that time, you could otherwise be travelling toward your destination meaning that having a stop or transfer is like having to travel extra distance which is why it makes sense to utilize INPs and NTPs at all. Finally, the reason we average our weighted sum at the end instead of something analogous such as summing our sums is because even though this would have a virtually identical result for our algorithm, by averaging our sums, it could give us a more normalized value across different subway systems (not just Seoul's).
         </MainText>
         <MainText>
-          There are a few alterations that can be made to this formula that might work better depending on the case. For instance, rather than averaging our weighted sums at the end, the sums can, themselves, be summed. To be completely honest, I don't really know which method would actually end up working better and maybe they'd both be essentially identical in effectiveness. Conceptually, there shouldn't really be much of a difference between if an average or summation is used; however, we will go with an average because an average should theoretically give a relatively normalized value across subways with different amounts of stations (at least when compared to a sum).
-        </MainText>
-        <MainText>
-          Additionally, one major problem is it's difficult to analyze the actual importance of certain routes. For example, even though some station, A, and another station, B, might both draw heavy traffic, this doesn't necessitate heavy traffic between A and B. A and B could be two hubs, so to speak, each with their own relatively contained subsystems. Unfortunately, since it's difficult to find data about the importance of actual routes themselves (unless we were to track people individually as they move around in a subway), the next best thing, if you ask me, is to just use station importance based on general traffic.
+          One major problem is it's difficult to analyze the actual importance of certain routes. For example, even though some station, A, and another station, B, might both draw heavy traffic, this doesn't necessitate heavy traffic between A and B. A and B could be two hubs, so to speak, each with their own relatively contained subsystems. Unfortunately, since it's difficult to find data about the importance of actual routes themselves (unless we were to track people individually as they move around in a subway), the next best thing, if you ask me, is to just use station importance based on general traffic.
         </MainText>
         <MainText>
           To summarize, the metric we're using is far from perfect, but its hard to make a perfect metric (and get data required for a perfect metric). Regardless, the method that has been devised to evaluate inefficiency should still be relatively effective as it can account for both distance and commuter time while providing a reasonable way to determine which to give more importance to.
         </MainText>
         <Subtitle>
-          Data Collection
+          Testing
         </Subtitle>
+        <MainText>
+          To test, I essentially created a random map containing 10 "stations." Each station was then turned into a vertex for a graph of the system. This was a dynamic graph on which a feed-forward NEAT network added edges that it also assigned to a certain line. I limited it's choice in lines to 5 options. This was honestly an arbitrary number; however, based on intuition, I don't think that 6+ lines would add any extra value to a system that only has 10 nodes (the Seoul Metro, with 434 stations, has 9 lines).
+        </MainText>
+        <Subtitle>
+          Data Collection (so far)
+        </Subtitle>
+        <MainText>
+          TL;DR: This is kinda a supplementary addition to this post regarding progress so far in applying this method to the Seoul Metro itself. Data collection regarding the Seoul Metro hasn't been completed yet. Furthermore, a more efficient architecture has to be devised for the neural net due to computational and hardware limitations.
+        </MainText>
         <MainText>
           To create a proper layout for a new Seoul subway system, we first need to find the actual locations of each station. It's entirely true that for a fully optimized system, it's also important to consider changing station locations; however, for our purposes, we'll just assume that it's only the paths that will be needing changing.
         </MainText>
@@ -80,20 +86,14 @@ class App extends React.Component {
           First, we defined the scope of what constituted the "Seoul subway system." Although it was a somewhat arbitrary decision, we limited what we considered "Seoul's subway" to just lines 1~9.
         </MainText>
         <MainText>
-          Following this, drawing from a list of each station in each line on Wikipedia, the latitude and longitude was found for each station with the help of Google Maps. This was compiled into a .csv file which we then converted into {'<'}x, y{'>'} coordinates via the Universal Transverse Mercator coordinate system. All of South Korea is within one UTM zone, so I hope that distortion is minimal, although there will of course be distortion. Either way, each station's new coordinates were compiled into a list and normalized within the range [0, 1].
+          Following this, drawing from a list of each station in each line on Wikipedia, the latitude and longitude was found for each station with the help of Google Maps. This was compiled into a .csv file which we then converted into {'<'}x, y{'>'} coordinates via the Universal Transverse Mercator coordinate system. All of South Korea is within one UTM zone, so I hope that distortion is minimal (although there will, of course, be distortion). Either way, each station's new coordinates were compiled into a list and normalized within the range [0, 1].
         </MainText>
         <SmallImg src={require('./utm_map.png')}/>
         <Caption>
           Using the coordinates we obtained, this is what we ended up with.
         </Caption>
         <MainText>
-          The other information that is necessary to create an efficient layout is to know each station's individual "importance." We used data regarding hourly traffic flow throughout the year at each stop in order to calculate each station's average hourly traffic. This worked as our "importance" score.
-        </MainText>
-        <Subtitle>
-          Implementation
-        </Subtitle>
-        <MainText>
-          Using a feed-forward NEAT algorithm, we created various "optimized" outputs. For our algorithm's input we first gave 868 "coordinate" inputs. Because we have 434 stations, to provide 2D coordinate information we added 93961 "connection" inputs which were set to 1 or 0 representing whether or not there was a connection between any two stations (93961 is ₄₃₄C₂).
+          The other information that is necessary to create an efficient layout is to know each station's individual "importance." We were able to find an official spreadsheet with the average throughput of each stations per hour throughout the year; however, we haven't scraped the data yet, but that's okay, because for now, we aren't going to be actually working with the Seoul Metro (maybe that'll be in a part 2 or 3).
         </MainText>
       </Post>
     );
@@ -137,7 +137,7 @@ const EntryTitle = styled.h1`
 
 const Subtitle = styled.h1`
   color: black;
-  font-size: 26px;
+  font-size: 30px;
   font-weight: 500;
   margin-left: 10%;
   margin-right: 10%;
@@ -148,13 +148,13 @@ const Caption = styled.p`
   color: #000000b5;
   font-size: 12px;
   font-weight: 200;
-  margin-top: 0px;
+  margin-top: 5px;
   text-align: center;
 `;
 
 const MainText = styled.h1`
   color: #000000b5;
-  font-size: 18px;
+  font-size: 20px;
   font-weight: 400;
   line-height: 26px;
   margin-left: 10%;
